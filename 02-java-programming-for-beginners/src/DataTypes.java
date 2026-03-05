fe70415d6584f4cd7067a511949bc50a5ff4bb2c
moro23 <billhab66@gmail.com>
2026-03-05 04:00:03 +0000

    add DataTypes class to demonstrate primitive data types in Java

---
+++ 02-java-programming-for-beginners/src/DataTypes.java
@@ -0,0 +1,40 @@
+public class DataTypes {
+    public static void main(String s[]){
+        
+        // lets define primitive data types 
+        //use for handling file datta in binary format 
+        byte b = 125; 
+        System.out.println("Byte value " + b);
+
+        //
+        short points = 3000; 
+        System.out.println("Points scored as 'short' value " + points); 
+
+        // 
+
+        int population = 43580000;
+        System.out.println("Population as 'int' value " + population);
+
+        // 
+        long age_in_milliseconds = 788923800000l; 
+        System.out.println("Age of 25 years in milliseconds as 'long' value " + age_in_milliseconds);
+
+        // 
+        char keyboard_input = 'a';
+        System.out.println("Key board input is: " + keyboard_input);
+
+        // 
+        boolean isLabCompleted = false;
+        System.out.println("The lab is completed " + isLabCompleted);
+
+        // 
+        float house_value = 40000000.58f; 
+        System.out.println("The value of the house is $" + house_value); 
+
+        // 
+        double pi = 3.14159265359; 
+        System.out.println("The value of pi is " + pi);
+
+
+    }
+}
