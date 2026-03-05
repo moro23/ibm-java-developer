1e242790a843a011d6437474f60d17646e72a5da
moro23 <billhab66@gmail.com>
2026-03-05 09:45:00 +0000

    add CharDataToInt class to demonstrate char to int conversion in Java

---
+++ 02-java-programming-for-beginners/src/CharDataToInt.java
@@ -0,0 +1,8 @@
+public class CharDataToInt {
+    public static void main(String args[]){
+        // declare a char var 
+        char var = 'a'; 
+        System.out.println("Initial Char var: " + var);
+        System.out.println("Convert Char var:" + (int)var);
+    }
+}
