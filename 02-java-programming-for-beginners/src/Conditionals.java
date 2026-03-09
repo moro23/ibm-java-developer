e321248f582e2e097ed725539e90b31652457aee
moro23 <billhab66@gmail.com>
2026-03-09 10:57:41 +0000

    add Conditionals class to demonstrate various conditional statements in Java

---
+++ 02-java-programming-for-beginners/src/Conditionals.java
@@ -0,0 +1,55 @@
+public class Conditionals {
+    public static void main(String[] args){
+
+        // implementing a single if condition 
+        // int number = 10; 
+        // if (number > 5){
+        //     System.out.println("The number is greater than 5");
+        // }
+
+        // implementing an if-else statement 
+        // int number = 3; 
+        // if (number > 5){
+        //     System.out.println("The number is greater than 5");
+        // }else{
+        //     System.out.println("The number is less than 5");
+        // }
+
+        // implementing a multiple conditions 
+        // int number = 3; 
+        // if (number > 5){
+        //     System.out.println("The number is greater than 5");
+        // }else if (number == 5){
+        //     System.out.println("The number is equals 5");
+        // }else{
+        //     System.out.println("The number is less than 5"); 
+        // }
+
+        // implementing a switch condition 
+        // int day = 10; 
+        // switch (day){
+        //     case 1: 
+        //         System.out.println("Moday");
+        //         break;
+        //     case 2: 
+        //         System.out.println("Tuesday");
+        //         break; 
+        //     case 3: 
+        //         System.out.println("Wednesday");
+        //         break; 
+        //     case 4: 
+        //         System.out.println("Thursday");
+        //         break; 
+        //     case 5: 
+        //         System.out.println("Friday");
+        //         break; 
+        //     case 6:
+        //         System.out.println("Saturday");
+        //         break; 
+        //     default:
+        //         System.out.println("Week day not included");
+        // }
+
+
+    }
+}
