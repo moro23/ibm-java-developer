01c79276f4ed9d3f698b8c1b57eb23dc6d143e7a
moro23 <billhab66@gmail.com>
2026-04-07 10:02:03 +0000

    add BookAccess class to compare prices of Book objects

---
+++ 03-oop-in-java/src/BookAccess.java
@@ -0,0 +1,41 @@
+
+
+public class BookAccess {
+
+    public static void comparePrices(Book book1, Book book2){
+        float book1Price = book1.getPrice(); 
+        float book2Price = book2.getPrice(); 
+        String priceCmp = ""; 
+        if (book1Price < book2Price){
+            // 
+            priceCmp = book1.getTitle() + " cost less than " + book2.getTitle();
+        }else if (book1Price == book2Price){
+            //
+            priceCmp = book1.getTitle() + " cost as much as " + book2.getTitle();
+        }else {
+            priceCmp = book1.getTitle() + " cost more than " + book2.getTitle();
+        }
+
+        // 
+        System.out.println(priceCmp);
+    }
+	public static void main(String s[]) {
+		Book book1 = new Book();
+		book1.setTitle("Atomic Habits");
+		book1.setAuthor("James Clear");
+		book1.setPrice(30.00f);
+
+		Book book2 = new Book();
+		book2.setTitle("Sapiens");
+		book2.setAuthor("Yuval Noah Harari");
+		book2.setPrice(25.00f);
+
+		System.out.println("The first book object is ");
+		System.out.println(book1);
+		System.out.println("The second book object is ");
+		System.out.println(book2);
+
+        // lets call the compare price methods 
+        comparePrices(book1, book2);
+	}
+}
