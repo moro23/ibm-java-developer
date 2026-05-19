8558f83db82a56d5d80558d571e556b07461d56a
moro23 <billhab66@gmail.com>
2026-05-19 11:32:18 +0000

    add ClassWithAbstractMeth class with abstract methods for further implementation

---
+++ 03-oop-in-java/src/ClassWithAbstractMeth.java
@@ -0,0 +1,8 @@
+public abstract class ClassWithAbstractMeth{
+
+    public abstract String absMeth1(int num); 
+    protected abstract boolean absMeth2(String str);
+    abstract float absMeth3(int num, String str);
+
+
+}
