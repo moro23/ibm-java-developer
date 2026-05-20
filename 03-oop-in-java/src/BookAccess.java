0856d32746d960f04be2691b2eb6763a18238bb2
moro23 <billhab66@gmail.com>
2026-05-20 06:26:50 +0000

    add new Book instance for demonstration in BookAccess class

--- 03-oop-in-java/src/BookAccess.java
+++ 03-oop-in-java/src/BookAccess.java
@@ -30,6 +30,9 @@ public class BookAccess {
 		book2.setAuthor("Yuval Noah Harari");
 		book2.setPrice(25.00f);
 
+        // 
+        Book book3 = new Book("Atomic Habits", "James Clear", 30.00f);
+
 		System.out.println("The first book object is ");
 		System.out.println(book1);
 		System.out.println("The second book object is ");
